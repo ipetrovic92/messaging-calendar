a291a1acab5fd5943b87399aa338c445a1542bb1
Ivan Petrovic <ivan42382@gmail.com>
2018-09-16 14:04:24 +0200

    Init commit with Spring security example

---
+++ src/main/java/com/ipetrovic/master/messagingcalendar/MessagingCalendarApplication.java
@@ -0,0 +1,12 @@
+package com.ipetrovic.master.messagingcalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingCalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagingCalendarApplication.class, args);
+	}
+}
